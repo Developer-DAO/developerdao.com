5c478df4b39d9b86c7edb68b9e4a8a1ca4b9c366
marcusdiaz <marcus.diaz@gmail.com>
2021-09-09 21:10:16 -0500

    Update src/components/Profile/Profile.js
    Co-authored-by: with-heart <with.heart+git@pm.me>

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -25,7 +25,7 @@ const Profile = (props) => {
             !props.loaded && (
               <>
               <button
-              onClick={props.connect}
+              onClick={props.onAuthenticate}
               className="pt-4 shadow-md bg-purple-800 mt-4 mb-2 text-white font-bold py-2 px-4 rounded"
             >Authenticate</button>
             
