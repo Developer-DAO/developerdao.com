d2a7fe1eeaea30feee5ec001084fb0cbea51ec2c
nafees nazik <nafeesnazik10@gmail.com>
2021-11-04 17:25:25 +0530

    refactor: persist dark mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
-
+import { ColorModeScript } from '@chakra-ui/react';
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -15,9 +15,18 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
-        <Head />
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/Inter-roman.var.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+        </Head>
         <body>
+          <ColorModeScript />
           <Main />
           <NextScript />
         </body>
