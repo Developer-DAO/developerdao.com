a0e7f09fc139efea239cb5e61e995899f2582491
nafees nazik <nafeesnazik10@gmail.com>
2021-11-04 17:22:39 +0530

    feat: replace preact with react in production for smaller bundle size

--- next.config.js
+++ next.config.js
@@ -3,4 +3,16 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   i18n,
   reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 };
