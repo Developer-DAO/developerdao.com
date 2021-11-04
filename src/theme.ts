2c777c2e8bb521b20218f376ca5dc905bfdc6d26
nafees nazik <nafeesnazik10@gmail.com>
2021-11-04 11:08:58 +0530

    refactor: update theme configs

--- src/theme.ts
+++ src/theme.ts
@@ -2,16 +2,44 @@ import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
   fonts: {
-    heading:
-      'InterVariable, -apple-system, BlinkMacSystemFont, system-ui, sans-serif',
-    body: 'InterVariable, -apple-system, BlinkMacSystemFont, system-ui, sans-serif',
-    code: '"Source Code Pro", Menlo, Monaco, Consolas, "Courier New", monospace',
+    heading: 'Inter var, sans-serif',
+    body: 'Inter var, sans-serif',
   },
   styles: {
     global: {
-      body: {
-        bg: 'gray.50',
+      'html, #__next': {
+        height: '100%',
       },
+      '#__next': {
+        display: 'flex',
+        flexDirection: 'column',
+      },
+      '.body': {
+        // todo check how to do this without breaking the site
+        // height: '100%', // Push footer to bottom
+        overflowY: 'scroll', // Always show scrollbar to avoid flickering
+      },
+      html: {
+        scrollBehavior: 'smooth',
+      },
+    },
+  },
+  textStyles: {
+    heading: {
+      fontFamily: 'heading',
+      textAlign: 'center',
+      fontWeight: 'bold',
+      letterSpacing: '-0.015em',
+      lineHeight: '1.24',
+      fontSize: { base: '2rem', md: '3.5rem' },
+    },
+    'heading-2': {
+      fontFamily: 'heading',
+      textAlign: 'center',
+      fontWeight: 'bold',
+      letterSpacing: '-0.015em',
+      lineHeight: '1.24',
+      fontSize: { base: '1.75rem', md: '2.75rem' },
     },
   },
 });
