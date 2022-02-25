a0a49364ff27c77b19941717a1cbc44b1dbb4c20
joneskj55 <joneskj55@gmail.com>
2022-02-25 16:02:56 -0600

    minor tweaks (spacing & responsiveness need work)

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,21 +1,19 @@
 import {
   Image,
-  Button,
   ButtonGroup,
-  Container,
-  IconButton,
+  Link,
   Stack,
   Text,
   Flex,
   useColorMode,
+  Box,
 } from '@chakra-ui/react';
-import * as React from 'react';
 import { FaTwitter, FaDiscord, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   const { colorMode } = useColorMode();
   return (
-    <Flex as="footer" role="contentinfo" alignItems={'center'} w={'full'}>
+    <Box as="footer" role="contentinfo" alignItems={'center'} w={'full'}>
       <Stack
         spacing="8"
         direction={{ base: 'column', md: 'row' }}
@@ -44,78 +42,85 @@ const Footer = () => {
           direction={['column', 'column', 'row']}
           spacing={{ base: '12', md: '8' }}
         >
-          <Stack minW="36" padding-bottom="5" flex="1">
+          <Stack minW="60" padding-bottom="5" spacing="6">
             <Text>Useful Links</Text>
             <Stack shouldWrapChildren>
-              <Button variant="link">Wiki</Button>
-              <Button variant="link">Forum</Button>
-              <Button variant="link">Snapshot</Button>
-              <Button variant="link">Podcast</Button>
-              <Button variant="link">Newsletter</Button>
+              <Link href="" isExternal>
+                Wiki
+              </Link>
+              <Link href="" isExternal>
+                Forum
+              </Link>
+              <Link href="" isExternal>
+                Snapshot
+              </Link>
+              <Link href="" isExternal>
+                Podcast
+              </Link>
+              <Link href="" isExternal>
+                Newsletter
+              </Link>
             </Stack>
           </Stack>
-          <Stack spacing="4" minW="36" flex="1">
-            <Text fontSize="sm" fontWeight="semibold" color="subtle">
-              Discover
-            </Text>
+
+          <Stack minW="40" padding-bottom="5" spacing="6">
+            <Text>Discover</Text>
             <Stack spacing="3" shouldWrapChildren>
-              <Button variant="link">Blog</Button>
-              <Button variant="link">Learning Resources</Button>
-              <Button variant="link">Projects</Button>
-              <Button variant="link">Partners</Button>
+              <Link href="" isExternal>
+                Blog
+              </Link>
+              <Link href="" isExternal>
+                Learning Resources
+              </Link>
+              <Link href="" isExternal>
+                Projects
+              </Link>
+              <Link href="" isExternal>
+                Partners
+              </Link>
             </Stack>
           </Stack>
-        </Stack>
-        <Stack spacing="4">
-          <Text fontSize="sm" fontWeight="semibold" color="subtle">
-            Social
-          </Text>
-          <Stack
-            spacing="4"
-            direction={{ base: 'column', sm: 'row' }}
-            maxW={{ lg: '360px' }}
-          >
-            <ButtonGroup variant="ghost">
-              <IconButton
-                as="a"
-                href="#"
-                aria-label="LinkedIn"
-                icon={<FaTwitter fontSize="1.25rem" />}
-              />
-              <IconButton
-                as="a"
-                href="#"
-                aria-label="GitHub"
-                icon={<FaDiscord fontSize="1.25rem" />}
-              />
-              <IconButton
-                as="a"
-                href="#"
-                aria-label="Twitter"
-                icon={<FaGithub fontSize="1.25rem" />}
-              />
-            </ButtonGroup>
+
+          <Stack minW="40" spacing="6">
+            <Text>Social</Text>
+            <Stack
+              spacing="4"
+              direction={{ base: 'column', sm: 'row' }}
+              maxW={{ lg: '360px' }}
+            >
+              <ButtonGroup>
+                <button>
+                  <FaTwitter href="#" aria-label="Twitter" />
+                </button>
+                <button>
+                  <FaDiscord href="#" aria-label="Discord" />
+                </button>
+                <button>
+                  <FaGithub href="#" aria-label="Github" />
+                </button>
+              </ButtonGroup>
+            </Stack>
           </Stack>
-        </Stack>
-        <Stack
-          pt="8"
-          pb="12"
-          justify="space-between"
-          direction={{ base: 'column-reverse', md: 'row' }}
-          align="center"
-        >
-          <Flex>
-            <Image
-              src={`/Blur-img-${colorMode === 'dark' ? 'light' : 'dark'}.svg`}
-              alt="Blur"
-            />
-            <Text fontSize="sm" ml={2}>
-              {new Date().getFullYear()} @ Developer DAO
-            </Text>
-          </Flex>
         </Stack>
       </Stack>
-    </Flex>
+
+      <Stack
+        pt="8"
+        justify="space-between"
+        direction={{ base: 'column-reverse', md: 'row' }}
+        align="center"
+      >
+        <Flex>
+          <Image
+            src={`/Blur-img-${colorMode === 'dark' ? 'light' : 'dark'}.svg`}
+            alt="Blur"
+          />
+          <Text fontSize="sm" ml={2}>
+            {new Date().getFullYear()} @ Developer DAO
+          </Text>
+        </Flex>
+      </Stack>
+    </Box>
   );
 };
 
