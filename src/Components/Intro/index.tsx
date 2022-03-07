251f38f1e27d16bebf5cb296de877c9823b2c727
Andrei Voinea <8058187+andreivdev@users.noreply.github.com>
2022-03-07 12:14:59 +0200

    Update index.tsx

--- src/Components/Intro/index.tsx
+++ src/Components/Intro/index.tsx
@@ -8,7 +8,9 @@ import {
   GridItem,
   Center,
 } from '@chakra-ui/react';
-import { BsFillLightningChargeFill } from 'react-icons/bs';
+import { BsFillLightningChargeFill, BsDiscord } from 'react-icons/bs';
+
+const beforeCODELaunch = false;
 
 const IntroComponent = () => {
   return (
@@ -19,49 +21,94 @@ const IntroComponent = () => {
         flexWrap="wrap"
         justifyContent="space-between"
       >
-        <Box
-          w={{ base: '100%', lg: '30%' }}
-          border="1px solid"
-          borderRadius="11px"
-          p="6"
-        >
-          <Text fontSize="54" fontFamily="Inter" fontWeight="800">
-            Build web3 with friends.
-          </Text>
-          <Text fontSize="24" fontFamily="Inter" fontWeight="500">
-            Developer DAO is a community of thousand of web3 builders creating a
-            better internet. Join us and create the future.
-          </Text>
-          <Box border="1px solid" borderRadius="11px" bg="white" p="6" mt="6">
-            <Text
-              fontSize="24"
-              fontFamily="Inter"
-              fontWeight="700"
-              color="black"
-            >
-              Current Status: Season 0
+        {beforeCODELaunch && (
+          <Box
+            w={{ base: '100%', lg: '30%' }}
+            border="1px solid"
+            borderRadius="11px"
+            p="6"
+          >
+            <Text fontSize="54" fontFamily="Inter" fontWeight="800">
+              Build web3 with friends.
             </Text>
-            <Text
-              fontSize="20"
-              fontFamily="Inter"
-              fontWeight="500"
-              color="black"
-            >
-              We&rsquo;re forming guilds, creating culture, strengthening our
-              community, teaching & learning, and building cool things together.
+            <Text fontSize="24" fontFamily="Inter" fontWeight="500">
+              Developer DAO is a community of thousand of web3 builders creating
+              a better internet. Join us and create the future.
             </Text>
-            <Button
-              mt="6"
-              p="6"
-              border="1px solid black"
-              borderRadius="11px"
-              color="black"
-            >
-              <Box as={BsFillLightningChargeFill} />
-              Read our snapshot
-            </Button>
+            <Box border="1px solid" borderRadius="11px" bg="white" p="6" mt="6">
+              <Text
+                fontSize="24"
+                fontFamily="Inter"
+                fontWeight="700"
+                color="black"
+              >
+                Current Status: Season 0
+              </Text>
+              <Text
+                fontSize="20"
+                fontFamily="Inter"
+                fontWeight="500"
+                color="black"
+              >
+                We&rsquo;re forming guilds, creating culture, strengthening our
+                community, teaching & learning, and building cool things
+                together.
+              </Text>
+              <Button
+                mt="6"
+                p="6"
+                border="1px solid black"
+                borderRadius="11px"
+                color="black"
+              >
+                <Box as={BsFillLightningChargeFill} />
+                Read our snapshot
+              </Button>
+            </Box>
           </Box>
-        </Box>
+        )}
+
+        {!beforeCODELaunch && (
+          <Box w={{ base: '100%', lg: '35%' }}>
+            <Text fontSize="86" fontFamily="Inter" fontWeight="800">
+              Build web3 with friends.
+            </Text>
+            <Text fontSize="24" fontFamily="Inter" fontWeight="500">
+              Developer DAO is a community of thousand of web3 builders creating
+              a better internet. Join us and create the future.
+            </Text>
+
+            <Flex justifyContent="center" w="100">
+              <Button
+                mt="6"
+                p="6"
+                border="1px solid black"
+                borderRadius="10px"
+                bg="white"
+                color="black"
+                fontSize="20"
+                flexGrow="1"
+              >
+                Claim $CODE
+              </Button>
+
+              <Button
+                mt="6"
+                p="6"
+                ml="6"
+                border="1px solid white"
+                borderRadius="10px"
+                bg="black"
+                color="white"
+                fontSize="20"
+                flexGrow="1"
+              >
+                <Box as={BsDiscord} mr="3" />
+                Join Discord
+              </Button>
+            </Flex>
+          </Box>
+        )}
         <Flex
           direction="column"
           w={{ base: '100%', lg: '30%' }}
