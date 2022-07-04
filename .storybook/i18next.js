f7a5d95eae09ac4dd69935977de70f371207df91
Alan P Currie <alanc909@outlook.com>
2022-07-04 18:06:14 +0100

    chakra ui addon update

--- .storybook/i18next.js
+++ .storybook/i18next.js
@@ -4,7 +4,7 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 const ns = ['common'];
-const supportedLngs = ['en'];
+const supportedLngs = ['en', 'fr'];
 const resources = ns.reduce((acc, n) => {
   supportedLngs.forEach((lng) => {
     if (!acc[lng]) acc[lng] = {};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../src/theme';
 import * as NextImage from 'next/image';
 import { RouterContext } from 'next/dist/shared/lib/router-context';
@@ -12,30 +11,24 @@ Object.defineProperty(NextImage, 'default', {
   value: (props) => <OriginalNextImage {...props} unoptimized />,
 });
 
-const withChakra = (StoryFn) => {
-  return (
-    <ChakraProvider theme={theme}>
-      <StoryFn />
-    </ChakraProvider>
-  );
-};
-
-export const decorators = [withChakra];
-
 export const parameters = {
+  chakra: {
+    theme,
+  },
   actions: {
     argTypesRegex: '^on[A-Z].*',
   },
   options: {
     storySort: { order: ['Introduction'] },
   },
+  chakra: {
+    theme,
+  },
   i18n,
   locale: 'en',
   locales: {
     en: 'English',
-  },
-  chakra: {
-    theme,
+    fr: 'French',
   },
   nextRouter: {
     Provider: RouterContext.Provider,
